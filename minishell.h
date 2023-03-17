3b14a6e0fcb626165924156dd4515f994dc3ec7e
Kdelphinus <kmj951015@gmail.com>
2023-03-17 14:03:58 +0900

    header 제거

--- minishell.h
+++ minishell.h
@@ -1,19 +1,5 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jaekkang <jaekkang@student.42.kr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/14 13:26:42 by jaekkang          #+#    #+#             */
-/*   Updated: 2023/03/17 14:01:57 by myko             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef MINISHELL_H
 # define MINISHELL_H
-
-# include <stdio.h>
 
 # include <readline/readline.h>
 # include <readline/history.h>

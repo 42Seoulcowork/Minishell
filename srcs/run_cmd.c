24e4c05665029efea068dc14368d10607ad3eed9
Kdelphinus <kmj951015@gmail.com>
2023-03-20 19:30:13 +0900

    run_cmd.c | fork() 전에 에러가 난다면 바로 종료하도록 수정

--- srcs/run_cmd.c
+++ srcs/run_cmd.c
@@ -10,23 +10,29 @@ void	run_cmd(char *cmd, t_envp tenvp)
 	tenvp.argc = 1;
 	tenvp.argv = malloc(sizeof(char *) * 2);
 	if (!tenvp.argv)
-		return ; // TODO 나중에 에러 처리 잘 해주세요
+		error(MALLOC_ERROR, "malloc", &tenvp) ; // TODO 나중에 에러 처리 잘 해주세요
 	tenvp.argv[0] = cmd;
 	tenvp.argv[1] = NULL;
 	new_argv = argv_init(0, tenvp);
 	if (!new_argv)
-		error(MALLOC_ERROR, "malloc");
+	{
+		error(MALLOC_ERROR, "malloc", &tenvp);
+		return ;
+	}
 	path = ft_path(new_argv[0], tenvp.paths);
 	if (!path)
-		error(COMMAND_ERROR, new_argv[0]);
+	{
+		error(COMMAND_ERROR, new_argv[0], &tenvp);
+		return;
+	}
 	pid = fork();
 	if (pid == 0)
 	{
 		if (execve(path, new_argv, tenvp.envp) == -1)
-			error(RUN_ERROR, new_argv[0]);
+			error(RUN_ERROR, new_argv[0], &tenvp);
 	}
 	else if (pid > 0)
 		waitpid(pid, &status, 0);
 	else
-		error(FORK_ERROR, "fork");
+		error(FORK_ERROR, "fork", &tenvp);
 }

c9626a1457cc6b596a838b0aff573f12fa3b84d1
Juyoung <juyojeon@student.42seoul.kr>
2023-03-28 22:47:59 +0900

    parser.h etc ... | name change

--- includes/parser.h
+++ includes/parser.h
@@ -13,44 +13,44 @@ typedef enum e_token_type
 	T_EOF,
 }	t_token_type;
 
-typedef enum e_redir_type
+typedef enum e_r_type
 {
 	RE_INPUT,
+	RE_HERE,
 	RE_OUTPUT,
 	RE_APPEND,
-	RE_HERE,
-}	t_redir_type;
+}	t_r_type;
 
 typedef struct s_redir
 {
-	int					target_fd;
-	char				*file_name;
-	enum e_redir_type	redir_type;
-	struct s_redir		*next;
+	int				target_fd;
+	char			*file_name;
+	enum e_r_type	redir_type;
+	struct s_redir	*next;
 }	t_redir;
 
 typedef struct s_token
 {
-	char				**cmd;
-	struct s_redir		*rdirs;
-	struct s_token		*next;
+	char			**cmd;
+	t_redir			*rdirs;
+	struct s_token	*next;
 }	t_token;
 
-typedef struct s_parsed_data
+typedef struct s_p_data
 {
 	int				size;
 	struct s_token	*front;
 	struct s_token	*rear;
-}	t_parsed_data;
+}	t_p_data;
 
-t_parsed_data	tokenize(char *input);
-t_parsed_data	*parsing(char *str);
-t_token			*create_token(char **cmd, t_redir *rdirs);
-t_token			*create_new_token(char *cmd, t_redir *rdirs);
+t_p_data	tokenize(char *input);
+t_p_data	*parsing(char *str);
+t_token		*create_token(char **cmd, t_redir *rdirs);
+t_token		*create_new_token(char *cmd, t_redir *rdirs);
 
-void			enqueue(t_parsed_data *data, t_token *token);
-t_token			*dequeue(t_parsed_data *data);
+void		enqueue(t_p_data *data, t_token *token);
+t_token		*dequeue(t_p_data *data);
 
-void			print_queue(t_parsed_data *queue);
+void		print_queue(t_p_data *queue);
 
 #endif

2df75d28a314be12f8a5b0a3e205f66172b4dfb1
Kdelphinus <kmj951015@gmail.com>
2023-03-30 18:13:18 +0900

    ft_exit.c | 종료상태 조금 바꿈

--- srcs/built_in/ft_exit.c
+++ srcs/built_in/ft_exit.c
@@ -27,8 +27,6 @@ static int	ft_atouc(const char *str)
 	i = 0;
 	sign = 1;
 	result = 0;
-	while ((9 <= str[i] && str[i] <= 13) || str[i] == ' ')
-		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
@@ -54,25 +52,24 @@ void	ft_exit(char **args)
 	int		exit_status;
 	char 	*msg;
 
-	printf("exit\n");
-	if (!args[1])
-	{
-		exit_status = 0;
-		exit(0);
-	}
-	else if (args[2])
-	{
-		ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
-		// 종료 상태 1로 변경
-		return ;
-	}
+	ft_putstr_fd("exit\n", STDERR_FILENO);
 	exit_status = ft_atouc(args[1]);
 	if (exit_status == -1)
 	{
 		msg = ft_strjoin(ft_strjoin("minishell: exit: ", args[1]), ": numeric argument required\n");
 		ft_putstr_fd(msg, STDERR_FILENO);
-		exit_status = 2;
+		exit_status = 255;
 	}
-	printf("exit status: %d\n", exit_status); // TODO 현재 종료 상태 확인, 출력이 아니라 종료 상태에 저장되도록 변경해야 함
+	else if (!args[1])
+	{
+		// 직전 종료상태로
+		exit(0);
+	}
+	else if (args[2]) // args[1]을 먼저 확인
+	{
+		ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
+		exit_status = 1;
+		return ;
+	}
 	exit(exit_status);
 }

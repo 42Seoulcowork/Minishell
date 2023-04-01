3d658f0b26d5833e9e557aa95217e76ed74c2496
Juyoung <juyojeon@student.42seoul.kr>
2023-04-01 22:45:01 +0900

    tokenize_process_2nd.c | under 25lines

--- srcs/parser/tokenize_process_2nd.c
+++ srcs/parser/tokenize_process_2nd.c
@@ -1,35 +1,40 @@
 #include "../../includes/minishell.h"
 
+static void	ft_handle_empty_word_and_expension_variable(char **input)
+{
+	int	i;
+
+	i = 1;
+	while ((*input)[i] == ' ')
+		++i;
+	if ((*input)[i] == '|')
+		(*input) += i;
+}
+
 void	ft_expension_process(char **input, t_word *word, t_env_node *node)
 {
 	char	*tmp;
-    int		i;
+	int		i;
 
-    i = -1;
+	i = -1;
 	if (*(*input + 1) == ' ' || *(*input + 1) == '|' || *(*input + 1) == '\n')
 		word->word[++(word->word_idx)] = '$';
 	else
-    {
-        (*input) += 1;
-        tmp = ft_strlen_for_exp(input);
-        while (node)
+	{
+		(*input) += 1;
+		tmp = ft_strlen_for_exp(input);
+		while (node)
 		{
-            if (ft_strcmp(tmp, node->key) == 0)
+			if (ft_strcmp(tmp, node->key) == 0)
 			{
-                while ((node->value)[++i])
-                    (word->word)[++(word->word_idx)] = node->value[i];
-                break;
-            }
-            node = node->next;
-        }
-        free(tmp);
-        if (node == NULL)
-        {
-            i = 1;
-            while ((*input)[i] == ' ')
-                ++i;
-            if ((*input)[i] == '|')
-                (*input) += i;
-        }
-    }
+				while ((node->value)[++i])
+					(word->word)[++(word->word_idx)] = node->value[i];
+				break;
+			}
+			node = node->next;
+		}
+		free(tmp);
+		if (node == NULL)
+			ft_handle_empty_word_and_expension_variable(input);
+	}
 }

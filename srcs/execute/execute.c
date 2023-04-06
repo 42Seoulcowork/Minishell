f9f2d5a7da22aebdae36cf0bac400e6c1c4accd2
gyuhong <qjwb2@naver.com>
2023-04-06 21:19:11 +0900

    execute.c | 함수 분리 시작

--- srcs/execute/execute.c
+++ srcs/execute/execute.c
@@ -1,57 +1,5 @@
 #include "minishell.h"
 
-static int	handle_redir(t_redir *redir)
-{
-	int fd;
-
-	while (redir != NULL)
-	{
-		if (redir->type == RE_INPUT)
-		{
-			fd = open(redir->file_name, O_RDONLY);
-			if (fd == -1) {
-				ft_putstr_fd("minishell: ", STDERR_FILENO);
-				ft_putstr_fd(redir->file_name, STDERR_FILENO);
-				if (!access(redir->file_name, F_OK))
-					ft_putstr_fd(": Permission denied\n", STDERR_FILENO);
-				else
-					ft_putstr_fd(": No such file or directory\n", STDERR_FILENO);
-				return (FALSE);
-			}
-			dup2(fd, STDIN_FILENO);
-			close(fd);
-		}
-		else if (redir->type == RE_OUTPUT) // TODO 권한 줬다뺐다 해봐
-		{
-			if (access(redir->file_name, W_OK) && !access(redir->file_name, F_OK))
-			{
-				ft_putstr_fd("minishell: ", STDERR_FILENO);
-				ft_putstr_fd(redir->file_name, STDERR_FILENO);
-				ft_putstr_fd(": Permission denied\n", STDERR_FILENO);
-				return (FALSE);
-			}
-			fd = open(redir->file_name, O_WRONLY | O_CREAT | O_TRUNC , 0644);
-			dup2(fd, STDOUT_FILENO);
-			close(fd);
-		}
-		else if (redir->type == RE_APPEND)
-		{
-			if (access(redir->file_name, W_OK) && !access(redir->file_name, F_OK))
-			{
-				ft_putstr_fd("minishell: ", STDERR_FILENO);
-				ft_putstr_fd(redir->file_name, STDERR_FILENO);
-				ft_putstr_fd(": Permission denied\n", STDERR_FILENO);
-				return (FALSE);
-			}
-			fd = open(redir->file_name, O_WRONLY | O_CREAT | O_APPEND, 0644);
-			dup2(fd, STDOUT_FILENO);
-			close(fd);
-		}
-		redir = redir->next;
-	}
-	return (TRUE);
-}
-
 void	run_builtin(t_env_node *head, t_token *token)
 {
 	if (token->cmd_type == ECHO_FUNC)
@@ -70,7 +18,7 @@ void	run_builtin(t_env_node *head, t_token *token)
 		ft_exit(token->cmd);
 }
 
-void		execute_token(t_env_node *head, t_token *token)
+void	execute_token(t_env_node *head, t_token *token)
 {
 	if (token->redir != NULL)
 	{
@@ -84,33 +32,174 @@ void		execute_token(t_env_node *head, t_token *token)
 		run_builtin(head, token);
 	else
 		run_cmd(head, token);
+}
 
+void	handle_execute_errror(int status)
+{
+	if (WIFEXITED(status))
+		g_exit_status = WEXITSTATUS(status);
+	else // TODO 시그널에 의해 종료되었거나 강제 종료되었을 때 처리가 필요함
+	{
+		g_exit_status = 130;
+		// TODO 주영이한테 물어봐요 규선, 명준, 수빈
+		if (WIFSIGNALED(status))
+		{
+			ft_putnbr_fd(WTERMSIG(status), 2);
+			ft_putstr_fd("  시그널에 의한 종료\n", 2);
+		}
+		else if (WIFSTOPPED(status))
+		{
+			// 130
+			ft_putnbr_fd(WSTOPSIG(status), 2);
+			ft_putstr_fd("  강제 종료에 의한 종료\n", 2);
+		}
+		else
+			ft_putstr_fd("하하하 왜 에러일까요, 알아맞춰주세요\n", 2);
+	}
+}
+
+static int	execute_no_pipe(t_env_node *head, t_p_data *p_data, int *status)
+{
+	pid_t	pid;
+
+	pid = 0;
+	if (p_data->front->cmd_type == EXTERN_FUNC)
+		pid = fork();
+	if (pid > 0)
+		wait(status);
+	else if (pid == 0)
+		execute_token(head, p_data->front);
+	else
+	{
+		perror("fork");
+		return (FALSE);
+	}
+	return (TRUE);
+}
+
+void	execute_first_pipe(t_env_node *head, t_p_data *p_data, int **fd)
+{
+	pid_t	pid;
+
+	if (pipe(fd[0]) == -1)
+	{
+		g_exit_status = 1;
+		perror("pipe");
+		return ;
+	}
+	pid = fork();
+	if (pid == 0)
+	{
+		close(fd[0][READ_END]);
+		dup2(fd[0][WRITE_END], STDOUT_FILENO);
+		close(fd[0][WRITE_END]);
+		execute_token(head, p_data->front);
+		exit(g_exit_status);
+	}
+	else if (pid > 0)
+	{
+		close(fd[0][WRITE_END]);
+		p_data->front = p_data->front->next;
+	}
+	else
+	{
+		g_exit_status = 1;
+		printf("fork error\n");
+	}
+}
+
+int	execute_middle_pipe(t_env_node *head, t_p_data *p_data, int **fd)
+{
+	int		i;
+	pid_t	pid;
+
+	i = 0;
+	while (i < p_data->pipe_cnt - 1)
+	{
+		++i;
+		if (pipe(fd[i]) == -1)
+		{
+			g_exit_status = 1;
+			perror("pipe");
+			return (-1);
+		}
+		pid = fork();
+		if (pid == 0) // 자식
+		{
+			close(fd[i][READ_END]);
+			dup2(fd[i - 1][READ_END], STDIN_FILENO);
+			close(fd[i - 1][READ_END]);
+			dup2(fd[i][WRITE_END], STDOUT_FILENO);
+			close(fd[i][WRITE_END]);
+			execute_token(head, p_data->front);
+			exit(g_exit_status);
+		}
+		else if (pid > 0)
+		{
+			close(fd[i - 1][READ_END]);
+			close(fd[i][WRITE_END]);
+			p_data->front = p_data->front->next;
+		}
+		else
+		{
+			g_exit_status = 1;
+			return (-1);
+		}
+	}
+	return (i);
+}
+
+int	execute_end_pipe(t_env_node *head, t_p_data *p_data, int **fd, int i)
+{
+	int		tmp;
+	int		status;
+	pid_t	pid;
+
+	pid = fork();
+	status = 0;
+	if (pid == 0) // 자식
+	{
+		dup2(fd[i][READ_END], STDIN_FILENO);
+		close(fd[i][READ_END]);
+		execute_token(head, p_data->front);
+		exit(g_exit_status);
+	}
+	else if (pid > 0)
+	{
+		close(fd[i][READ_END]);
+		i = 0;
+		while (i < p_data->pipe_cnt + 1)
+		{
+			if (pid == wait(&tmp))
+				status = tmp;
+			++i;
+		}
+	}
+	else
+	{
+		g_exit_status = 1;
+		perror("fork");
+	}
+	return (status);
 }
 
 void	execute(t_env_node *head, t_p_data *p_data)
 {
 	int	stdin_dup;
 	int	stdout_dup;
-	int **fd;
-	int i;
-	int	tmp;
-	int status;
-	pid_t pid;
+	int	**fd;
+	int	i;
+	int	status;
 
+	i = 0;
 	stdin_dup = dup(STDIN_FILENO);
 	stdout_dup = dup(STDOUT_FILENO);
-	i = 0;
-	pid = 0;
 	if (p_data->pipe_cnt == 0)
 	{
-		if (p_data->front->cmd_type == EXTERN_FUNC)
-			pid = fork();
-		if (pid > 0)
-			wait(&status);
-		else if (pid == 0)
-			execute_token(head, p_data->front);
-		else
-			perror("fork");
+		if (execute_no_pipe(head, p_data, &status) == FALSE)
+			return ;
+		if (p_data->front->cmd_type != EXTERN_FUNC)
+			return ;
 	}
 	else
 	{
@@ -120,102 +209,10 @@ void	execute(t_env_node *head, t_p_data *p_data)
 			fd[i] = malloc(sizeof(int) * 2);
 			++i;
 		}
-		if (pipe(fd[0]) == -1)
-		{
-			g_exit_status = 1;
-			// error 처리
-		}
-		pid = fork();
-		if (pid == 0)
-		{
-			close(fd[0][READ_END]);
-			dup2(fd[0][WRITE_END], STDOUT_FILENO);
-			close(fd[0][WRITE_END]);
-			execute_token(head, p_data->front);
-			exit(g_exit_status);
-		}
-		else if (pid > 0)
-		{
-			close(fd[0][WRITE_END]);
-			p_data->front = p_data->front->next;
-		}
-		else
-		{
-			g_exit_status = 1;
-			printf("fork error\n");
-		}
-		i = 0;
-		while (i < p_data->pipe_cnt - 1)
-		{
-			++i;
-			pipe(fd[i]);//todo pipe error
-			pid = fork();
-			if (pid == 0) // 자식
-			{
-				close(fd[i][READ_END]);
-				dup2(fd[i - 1][READ_END], STDIN_FILENO);
-				close(fd[i - 1][READ_END]);
-				dup2(fd[i][WRITE_END], STDOUT_FILENO);
-				close(fd[i][WRITE_END]);
-				execute_token(head, p_data->front);
-				exit(g_exit_status);
-			}
-			else if (pid > 0)
-			{
-				close(fd[i - 1][READ_END]);
-				close(fd[i][WRITE_END]);
-				p_data->front = p_data->front->next;
-			}
-			else
-			{
-				g_exit_status = 1;
-				// error
-			}
-		}
-		pid = fork();
-		if (pid == 0) // 자식
-		{
-			dup2(fd[i][READ_END], STDIN_FILENO);
-			close(fd[i][READ_END]);
-			execute_token(head, p_data->front);
-			exit(g_exit_status);
-		}
-		else if (pid > 0)
-		{
-			close(fd[i][READ_END]);
-			i = 0;
-			while (i < p_data->pipe_cnt + 1)
-			{
-				if (pid == wait(&tmp))
-					status = tmp;
-				++i;
-			}
-		}
-		else
-		{
-			g_exit_status = 1;
-			printf("fork error\n");
-		}
-		if (WIFEXITED(status))
-			g_exit_status = WEXITSTATUS(status);
-		else // TODO 시그널에 의해 종료되었거나 강제 종료되었을 때 처리가 필요함
-		{
-			g_exit_status = 130;
-			// TODO 주영이한테 물어봐요 규선, 명준, 수빈
-			if (WIFSIGNALED(status))
-			{
-				ft_putnbr_fd(WTERMSIG(status), 2);
-				ft_putstr_fd("  시그널에 의한 종료\n", 2);
-			}
-			else if (WIFSTOPPED(status))
-			{
-				// 130
-				ft_putnbr_fd(WSTOPSIG(status), 2);
-				ft_putstr_fd("  강제 종료에 의한 종료\n", 2);
-			}
-			else
-				ft_putstr_fd("하하하 왜 에러일까요, 알아맞춰주세요\n", 2);
-		}
+		execute_first_pipe(head, p_data, fd);
+		i = execute_middle_pipe(head, p_data, fd);
+		status = execute_end_pipe(head, p_data, fd, i);
+		handle_execute_errror(status);
 	}
 	dup2(stdin_dup, STDIN_FILENO);
 	dup2(stdout_dup, STDOUT_FILENO);

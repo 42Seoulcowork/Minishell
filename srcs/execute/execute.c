571b20d6e5a852ed8dbe67dd7af18dee336fa634
gyuhong <qjwb2@naver.com>
2023-04-06 21:56:50 +0900

    execute.c | norm 작업

--- srcs/execute/execute.c
+++ srcs/execute/execute.c
@@ -1,6 +1,6 @@
 #include "minishell.h"
 
-void	run_builtin(t_env_node *head, t_token *token)
+static void	run_builtin(t_env_node *head, t_token *token)
 {
 	if (token->cmd_type == ECHO_FUNC)
 		ft_echo(token->cmd);
@@ -34,7 +34,7 @@ void	execute_token(t_env_node *head, t_token *token)
 		run_cmd(head, token);
 }
 
-void	handle_execute_errror(int status)
+static void	handle_execute_errror(int status)
 {
 	if (WIFEXITED(status))
 		g_exit_status = WEXITSTATUS(status);
@@ -58,148 +58,17 @@ void	handle_execute_errror(int status)
 	}
 }
 
-static int	execute_no_pipe(t_env_node *head, t_p_data *p_data, int *status)
+int	execute_p_data(t_env_node *head, t_p_data *p_data, int i)
 {
-	pid_t	pid;
-
-	pid = 0;
-	if (p_data->front->cmd_type == EXTERN_FUNC)
-		pid = fork();
-	if (pid > 0)
-		wait(status);
-	else if (pid == 0)
-		execute_token(head, p_data->front);
-	else
-	{
-		perror("fork");
-		return (FALSE);
-	}
-	return (TRUE);
-}
-
-void	execute_first_pipe(t_env_node *head, t_p_data *p_data, int **fd)
-{
-	pid_t	pid;
-
-	if (pipe(fd[0]) == -1)
-	{
-		g_exit_status = 1;
-		perror("pipe");
-		return ;
-	}
-	pid = fork();
-	if (pid == 0)
-	{
-		close(fd[0][READ_END]);
-		dup2(fd[0][WRITE_END], STDOUT_FILENO);
-		close(fd[0][WRITE_END]);
-		execute_token(head, p_data->front);
-		exit(g_exit_status);
-	}
-	else if (pid > 0)
-	{
-		close(fd[0][WRITE_END]);
-		p_data->front = p_data->front->next;
-	}
-	else
-	{
-		g_exit_status = 1;
-		printf("fork error\n");
-	}
-}
-
-int	execute_middle_pipe(t_env_node *head, t_p_data *p_data, int **fd)
-{
-	int		i;
-	pid_t	pid;
-
-	i = 0;
-	while (i < p_data->pipe_cnt - 1)
-	{
-		++i;
-		if (pipe(fd[i]) == -1)
-		{
-			g_exit_status = 1;
-			perror("pipe");
-			return (-1);
-		}
-		pid = fork();
-		if (pid == 0) // 자식
-		{
-			close(fd[i][READ_END]);
-			dup2(fd[i - 1][READ_END], STDIN_FILENO);
-			close(fd[i - 1][READ_END]);
-			dup2(fd[i][WRITE_END], STDOUT_FILENO);
-			close(fd[i][WRITE_END]);
-			execute_token(head, p_data->front);
-			exit(g_exit_status);
-		}
-		else if (pid > 0)
-		{
-			close(fd[i - 1][READ_END]);
-			close(fd[i][WRITE_END]);
-			p_data->front = p_data->front->next;
-		}
-		else
-		{
-			g_exit_status = 1;
-			return (-1);
-		}
-	}
-	return (i);
-}
-
-int	execute_end_pipe(t_env_node *head, t_p_data *p_data, int **fd, int i)
-{
-	int		tmp;
-	int		status;
-	pid_t	pid;
-
-	pid = fork();
-	status = 0;
-	if (pid == 0) // 자식
-	{
-		dup2(fd[i][READ_END], STDIN_FILENO);
-		close(fd[i][READ_END]);
-		execute_token(head, p_data->front);
-		exit(g_exit_status);
-	}
-	else if (pid > 0)
-	{
-		close(fd[i][READ_END]);
-		i = 0;
-		while (i < p_data->pipe_cnt + 1)
-		{
-			if (pid == wait(&tmp))
-				status = tmp;
-			++i;
-		}
-	}
-	else
-	{
-		g_exit_status = 1;
-		perror("fork");
-	}
-	return (status);
-}
-
-void	execute(t_env_node *head, t_p_data *p_data)
-{
-	int	stdin_dup;
-	int	stdout_dup;
 	int	**fd;
-	int	i;
 	int	status;
 
-	i = 0;
-	stdin_dup = dup(STDIN_FILENO);
-	stdout_dup = dup(STDOUT_FILENO);
 	if (p_data->pipe_cnt == 0)
 	{
 		if (execute_no_pipe(head, p_data, &status) == FALSE)
-			return ;
+			return (FALSE);
 		if (p_data->front->cmd_type != EXTERN_FUNC)
-			return ;
+			return (FALSE);
 	}
 	else
 	{
@@ -212,8 +81,20 @@ void	execute(t_env_node *head, t_p_data *p_data)
 		execute_first_pipe(head, p_data, fd);
 		i = execute_middle_pipe(head, p_data, fd);
 		status = execute_end_pipe(head, p_data, fd, i);
-		handle_execute_errror(status);
 	}
+	handle_execute_errror(status);
+	return (TRUE);
+}
+
+void	execute(t_env_node *head, t_p_data *p_data)
+{
+	int	stdin_dup;
+	int	stdout_dup;
+
+	stdin_dup = dup(STDIN_FILENO);
+	stdout_dup = dup(STDOUT_FILENO);
+	if (execute_p_data(head, p_data, 0) == FALSE)
+		return ;
 	dup2(stdin_dup, STDIN_FILENO);
 	dup2(stdout_dup, STDOUT_FILENO);
 	close(stdin_dup);

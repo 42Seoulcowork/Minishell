bfc42e90c4a90330b4231b622e0d8bc7c8abccdc
subillie <subinlee@c1r6s2.42seoul.kr>
2023-04-07 23:00:21 +0900

    ft_echo.c | 옵션 관련 구현

--- srcs/built_in/ft_echo.c
+++ srcs/built_in/ft_echo.c
@@ -1,16 +1,16 @@
 #include "minishell.h"
 
-static int	check_echo_option(char **args)
+static int	check_echo_option(char *arg)
 {
 	int	i;
 
 	i = 0;
-	if (args[1][0] == '-' && args[1][1] == 'n')
+	if (arg[0] == '-' && arg[1] == 'n')
 	{
 		i = 1;
-		while (args[1][i] == 'n')
+		while (arg[i] == 'n')
 			i++;
-		if (args[1][i] != '\0')
+		if (arg[i] != '\0')
 			return (0);
 	}
 	return (i);
@@ -20,7 +20,9 @@ void	ft_echo(char **args)
 {
 	int	i;
 	int	j;
+	int	no_option;
 	int	option_flag;
+	int	newline_flag;
 
 	g_exit_status = 0;
 	if (!args[1])
@@ -28,18 +30,25 @@ void	ft_echo(char **args)
 		ft_putchar_fd('\n', STDOUT_FILENO);
 		return ;
 	}
-	option_flag = check_echo_option(args);
 	i = 0;
-	if (option_flag > 0)
-		i = 1;
+	no_option = 0;
+	newline_flag = 0;
 	while (args[++i])
 	{
-		j = -1;
-		while (args[i][0] != '-' && args[i][++j])
-			write(STDOUT_FILENO, &args[i][j], 1);
-		if (args[i + 1])
-			write(STDOUT_FILENO, " ", 1);
+		if (no_option == 0)
+			option_flag = check_echo_option(args[i]);
+		if (option_flag == 0)
+		{
+			j = -1;
+			while (args[i][++j])
+				write(STDOUT_FILENO, &args[i][j], 1);
+			if (args[i + 1])
+				write(STDOUT_FILENO, " ", 1);
+			no_option = 1;
+		}
+		else
+			newline_flag = 1;
 	}
-	if (!option_flag)
+	if (newline_flag == 0)
 		write(STDOUT_FILENO, "\n", 1);
 }

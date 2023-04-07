beb087019af9ede2309215f4d711ef70e61f86f1
gyuhong <qjwb2@naver.com>
2023-04-07 21:20:33 +0900

    srcs/execute/ft_pwd | stderr -> stdout

--- srcs/built_in/ft_pwd.c
+++ srcs/built_in/ft_pwd.c
@@ -18,7 +18,7 @@ void	ft_pwd(t_env_node *head)
 		tmp = ft_getenv(head, "PWD");
 		path = ft_strjoin_s(tmp, "\n");
 		free(tmp);
-		ft_putstr_fd(path, STDERR_FILENO);
+		ft_putstr_fd(path, STDOUT_FILENO);
 		g_exit_status = 0;
 	}
 }

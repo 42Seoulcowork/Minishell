1e8e4a4024ead78933af629c6a5b6ae64fbcdcb4
Juyoung <juyojeon@student.42seoul.kr>
2023-04-08 03:50:44 +0900

    parsing.c | call by reference

--- srcs/parser/parsing.c
+++ srcs/parser/parsing.c
@@ -22,33 +22,33 @@ void	parsing(char *input, t_p_data *pdata, t_env_node *head)
 	ft_init_token_word(pdata, &word);
 	while (word.break_flag == OFF)
 	{
-		tokenize(input, pdata, &word, head);
+		tokenize(&input, pdata, &word, head);
 		++input;
 	}
 }
 
-void	tokenize(char *input, t_p_data *pdata, t_word *word, t_env_node *head)
+void	tokenize(char **input, t_p_data *pdata, t_word *word, t_env_node *head)
 {
-	if (*input == '\0')
+	if (**input == '\0')
 		ft_end_line_fin_hpwtt(pdata, word, head);
-	else if (word->dq_stt == OFF && word->sq_stt == OFF && *input == '|')
+	else if (word->dq_stt == OFF && word->sq_stt == OFF && **input == '|')
 		ft_add_new_token_hpwtt(pdata, word, head);
-	else if ((word->sq_stt == ON && *input == '\'')
-		|| (word->dq_stt == ON && *input == '\"'))
-		ft_end_quoted_stt(*input, word);
-	else if ((word->sq_stt == OFF && *input == '\'')
-		|| (word->dq_stt == OFF && *input == '\"'))
-		ft_start_quoted_stt(*input, word);
-	else if (word->sq_stt == OFF && *input == '$')
-		ft_start_expansion_stt(&input, word, head);
+	else if ((word->sq_stt == ON && **input == '\'')
+		|| (word->dq_stt == ON && **input == '\"'))
+		ft_end_quoted_stt(**input, word);
+	else if ((word->sq_stt == OFF && word->dq_stt == OFF)
+		&& (**input == '\'' || **input == '\"'))
+		ft_start_quoted_stt(**input, word, head);
+	else if (word->sq_stt == OFF && **input == '$')
+		ft_start_expansion_stt(input, word, head);
 	else if (word->dq_stt == OFF && word->sq_stt == OFF
-		&& (*input == '<' || *input == '>'))
-		ft_start_redirect_stt(pdata, *input, word, head);
-	else if (word->dq_stt == OFF && word->sq_stt == OFF && *input == ' ')
+		&& (**input == '<' || **input == '>'))
+		ft_start_redirect_stt(pdata, **input, word, head);
+	else if (word->dq_stt == OFF && word->sq_stt == OFF && **input == ' ')
 		ft_clean_new_word_hpwtt(pdata, word, head);
-	else if (*input == '#' && word->word[0] == '\0'
+	else if (**input == '#' && word->word[0] == '\0'
 		&& word->dq_stt == OFF && word->sq_stt == OFF)
 		ft_handle_comment(word);
 	else
-		ft_add_or_start_new_char_in_word(*input, word);
+		ft_add_or_start_new_char_in_word(**input, word, head);
 }

f2f792c149cbee566795140382a06ff99ba5d5cd
gyuhong <qjwb2@naver.com>
2023-04-07 15:08:28 +0900

    srcs/built_in/ft_export.c | 놈 맞추는 중...

--- srcs/built_in/ft_export.c
+++ srcs/built_in/ft_export.c
@@ -1,70 +1,12 @@
 #include "minishell.h"
 
-static int	arr_len(char **arr)
-{
-	int	i;
-
-	i = 0;
-	while (arr[i])
-		i++;
-	return (i);
-}
-
-static t_env_node	*get_old_key_address(t_env_node *head, char *key)
-{
-	head = head->next;
-	while (head != NULL)
-	{
-		if (ft_strcmp(head->key, key) == 0)
-			return (head);
-		head = head->next;
-	}
-	return (NULL);
-}
-
-static char	**ft_simple_split(char *tmp, char *equal_address)
-{
-	char	**str;
-	int		i;
-	int		j;
-
-	i = 0;
-	str = malloc(sizeof(char *) * 3);
-	str[0] = malloc(sizeof(char) * (equal_address - tmp + 1));
-	str[1] = malloc(sizeof(char) * (ft_strlen(tmp) - (equal_address - tmp) + 1));
-	str[2] = NULL;
-	while (tmp + i < equal_address)
-	{
-		str[0][i] = tmp[i];
-        ++i;
-    }
-    str[0][i] = '\0';
-    if (str[0][i - 1] == '+')
-        str[0][i - 1] = '\0';
-    j = 0;
-	++i;
-	while (*(tmp + i) != '\0')
-	{
-		str[1][j] = tmp[i];
-		++i;
-		++j;
-	}
-	str[1][j] = '\0';
-	return (str);
-}
-
 static int	is_valid_name(char *cmd, int *is_addition_assignment)
 {
 	int	i;
 
 	i = 0;
 	if (!ft_isalpha(cmd[i]) && cmd[i] != '_')
-	{
-		ft_putstr_fd("minishell: export: `", STDERR_FILENO);
-		ft_putstr_fd(cmd, STDERR_FILENO);
-		ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
-		return (0);
-	}
+		return (print_error_for_invalid_name(cmd));
 	++i;
 	while (cmd[i] != '\0' && cmd[i] != '=')
 	{
@@ -73,40 +15,66 @@ static int	is_valid_name(char *cmd, int *is_addition_assignment)
             *is_addition_assignment = 1;
             return (1);
         }
-		else if (!ft_isalnum(cmd[i]) && cmd[i] != '_')
-		{
-			ft_putstr_fd("minishell: export: `", STDERR_FILENO);
-			ft_putstr_fd(cmd, STDERR_FILENO);
-			ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
-			return (0);
-		}
+		if (!ft_isalnum(cmd[i]) && cmd[i] != '_')
+			return (print_error_for_invalid_name(cmd));
 		++i;
 	}
 	return (1);
 }
 
+static void	export_with_equal(t_env_node *head, char *tmp, char *cmd, int is_addition_assignment)
+{
+	char		**arr;
+	t_env_node	*old_key;
+
+	if (*(tmp + 1) != '\0')
+	{
+		arr = ft_simple_split(cmd, tmp);
+		old_key = get_old_key_address(head, arr[0]);
+		if (old_key == NULL)
+			add_node(head, create_node(arr[0], arr[1]));
+		else if (is_addition_assignment)
+		{
+			tmp = ft_strjoin(old_key->value, arr[1]);
+			free(old_key->value);
+			old_key->value = tmp;
+		}
+		else
+			old_key->value = arr[1];
+	}
+	else
+	{
+		*tmp = '\0';
+		if (is_addition_assignment)
+			*(tmp - 1) = '\0';
+		old_key = get_old_key_address(head, cmd);
+		if (old_key == NULL)
+			add_node(head, create_node(cmd, ft_strdup("")));
+		else if (!is_addition_assignment)
+			old_key->value = "";
+	}
+}
+
+static void	export_without_equal(t_env_node *head, char *cmd)
+{
+	t_env_node	*old_key;
+
+	old_key = get_old_key_address(head, cmd);
+	if (old_key == NULL)
+		add_node(head, create_node(cmd, NULL));
+
+}
+
 void	ft_export(t_env_node *head, char **cmd)
 {
 	int			i;
-	char		**arr;
 	char		*tmp;
-	t_env_node	*old_key;
     int         is_addition_assignment;
 
-	i = 0;
     is_addition_assignment = 0;
 	g_exit_status = 0;
 	if (cmd[1] == NULL)
-	{
-		arr = convert_array_for_export(head);
-		quick_sort(arr, 0, arr_len(arr) - 1);
-		while (arr[i])
-		{
-			printf("declare -x %s\n", arr[i]);
-			++i;
-		}
-		return ;
-	}
+		return (print_export(head));
 	i = 1;
 	while (cmd[i])
 	{
@@ -114,44 +82,13 @@ void	ft_export(t_env_node *head, char **cmd)
 		if (!is_valid_name(cmd[i], &is_addition_assignment))
 		{
 			++i;
-			head->value = "1";
+			g_exit_status = 1;
 			continue ;
 		}
 		if (tmp == NULL)
-		{
-			old_key = get_old_key_address(head, cmd[i]);
-			if (old_key == NULL)
-				add_node(head, create_node(cmd[i], NULL));
-		}
+			export_without_equal(head, cmd[i]);
 		else
-		{
-			if (*(tmp + 1) != '\0')
-			{
-				arr = ft_simple_split(cmd[i], tmp);
-				old_key = get_old_key_address(head, arr[0]);
-				if (old_key == NULL)
-					add_node(head, create_node(arr[0], arr[1]));
-                else if (is_addition_assignment)
-                {
-                    tmp = ft_strjoin(old_key->value, arr[1]);
-                    free(old_key->value);
-                    old_key->value = tmp;
-                }
-				else
-					old_key->value = arr[1];
-			}
-			else
-			{
-				*tmp = '\0';
-                if (is_addition_assignment)
-                    *(tmp - 1) = '\0';
-				old_key = get_old_key_address(head, cmd[i]);
-				if (old_key == NULL)
-					add_node(head, create_node(cmd[i], ft_strdup("")));
-				else if (!is_addition_assignment)
-					old_key->value = "";
-			}
-		}
+			export_with_equal(head, tmp, cmd[i], is_addition_assignment);
 		++i;
 	}
 }

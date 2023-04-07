378bcbf29b560b052040db805a7045df72c5825c
Juyoung <juyojeon@student.42seoul.kr>
2023-04-07 21:30:36 +0900

    tokenize_4th.c | syntax_error return type change

--- srcs/parser/tokenize_process_4th.c
+++ srcs/parser/tokenize_process_4th.c
@@ -1,16 +1,20 @@
 #include "minishell.h"
 
-static void	ft_here_doc_pretreatment(t_redir *new, t_word *word)
+static int	ft_here_doc_pretreatment(t_p_data *pdata, \
+t_redir *new, t_word *word)
 {
 	char	*str;
 	char	*tmp;
+	char	temp;
 
-	new->type = RE_HERE;
 	word->re_idx += 1;
-	if (word->word[word->re_idx] == '>' || word->word[word->re_idx] == '<' \
-	|| word->word[word->re_idx] == '(' || word->word[word->re_idx] == ')' \
-	|| word->word[word->re_idx] == '`' || word->word[word->re_idx] == '&')
-		ft_stx_near_unexp_tk_error();
+	temp = word->word[word->re_idx];
+	if (temp == '>' || temp == '<' || temp == '(' || temp == ')' 
+	|| temp == '`' || temp == '&' || temp == '\0')
+	{
+		ft_syntax_error(pdata, word);
+		return (1);
+	}
 	str = ft_itoa(++(word->tmp_num));
 	if (!str)
 		ft_allocation_error();
@@ -23,14 +27,16 @@ static void	ft_here_doc_pretreatment(t_redir *new, t_word *word)
 		ft_open_error();
 	unlink(tmp);
 	free(tmp);
+	return (0);
 }
 
-void	ft_redirect_here_doc(t_p_data *pdata, t_redir *new, t_word *word)
+int	ft_redirect_here_doc(t_p_data *pdata, t_redir *new, t_word *word)
 {
 	char	*tmp;
 	char	*str;
 
-	ft_here_doc_pretreatment(new, word);
+	if (ft_here_doc_pretreatment(pdata, new, word) == 1)
+		return (1);
 	tmp = ft_strdup(word->word + word->re_idx);
 	if (!tmp)
 		ft_allocation_error();
@@ -39,16 +45,14 @@ void	ft_redirect_here_doc(t_p_data *pdata, t_redir *new, t_word *word)
 		str = readline("> ");
 		if (!str)
 			ft_clear_all_pdata(pdata);
-		if (ft_strcmp(tmp, str) == 0)
+		if (!str || ft_strcmp(tmp, str) == 0)
 		{
 			free(str);
 			break ;
 		}
-		else
-		{
-			write(new->heredoc_fd, str, ft_strlen(str));
-			write(new->heredoc_fd, "\n", 1);
-		}
+		write(new->heredoc_fd, str, ft_strlen(str));
+		write(new->heredoc_fd, "\n", 1);
 		free(str);
 	}
+	return (0);
 }

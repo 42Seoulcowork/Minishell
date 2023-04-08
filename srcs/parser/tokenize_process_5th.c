736790bddb12d946bf75c9c193f29621f0802cdf
Juyoung <juyojeon@student.42seoul.kr>
2023-04-08 15:15:29 +0900

    tokenize_process_th | syntax_error

--- srcs/parser/tokenize_process_5th.c
+++ srcs/parser/tokenize_process_5th.c
@@ -20,12 +20,6 @@ void	ft_clean_new_word_hpwtt(t_p_data *pdata, t_word *word, t_env_node *node)
 			return ;
 	}
 	ft_handle_present_w_cmd_to_token(pdata, word);
-	if (pdata->now->cmd == NULL && pdata->now->redir == NULL
-		&& word->ex_idx == -1)
-	{
-		ft_syntax_error(pdata, word);
-		return ;
-	}
 	ft_clear_word_struct(word);
 }
 

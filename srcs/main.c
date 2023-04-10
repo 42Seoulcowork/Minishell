a19ebfd418fbb4e074908807150a83ad2bac168f
Juyoung Jeong <wdd1016@gmail.com>
2023-04-10 23:49:45 +0900

    main.c | init signal function

--- srcs/main.c
+++ srcs/main.c
@@ -9,6 +9,13 @@ static t_env_node	*initialize(int ac, char **av, char **envp)
 	return (init_node(envp));
 }
 
+int	ft_print_exit_free_env_all(t_env_node *head)
+{
+	write(1, "exit\n", 5);
+	exit(0);
+	return (0);
+}
+
 int	main(int ac, char **av, char **envp)
 {
 	char		*str;
@@ -16,21 +23,19 @@ int	main(int ac, char **av, char **envp)
 	t_env_node	*head;
 
 	head = initialize(ac, av, envp);
-	//signal_setting();
+	ft_signal_init();
 	while (1)
 	{
 		str = readline("minishell$ ");
 		if (!str)
-		{
-			printf("exit\n");
-			break ;
-		}
+			return (ft_print_exit_free_env_all(head));
+		ft_signal_init();
+		if (str[0] != '\0')
+			add_history(str);
 		parsing(str, &parsed_data, head->next);
 		if (parsed_data.front && (parsed_data.front->cmd || parsed_data.front->redir))
 		{
 			execute(head, &parsed_data);
-			if (str[0] != '\0')
-				add_history(str);
 			printf("exit_status: %d\n", g_exit_status);
 		}
 		free(str);

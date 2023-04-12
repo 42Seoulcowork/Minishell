8b7b7da70f1f41698311e15a2d52ec194fbea068
gyuhong <qjwb2@naver.com>
2023-04-12 21:34:39 +0900

    execute.c | fd 할당한 것들 다 free처리

--- srcs/execute/execute.c
+++ srcs/execute/execute.c
@@ -75,6 +75,13 @@ void	execute_p_data(t_env_node *head, t_p_data *p_data, int i)
 		execute_first_pipe(head, p_data, fd);
 		i = execute_middle_pipe(head, p_data, fd);
 		status = execute_end_pipe(head, p_data, fd, i);
+		i = 0;
+		while (i < p_data->pipe_cnt)
+		{
+			free(fd[i]);
+			++i;
+		}
+		free(fd);
 	}
 	handle_execute_errror(status);
 }

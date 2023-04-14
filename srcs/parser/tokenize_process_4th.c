573225ba6cc2d6c05320f78ad49b431acaa2c879
Kdelphinus <kmj951015@gmail.com>
2023-04-14 20:56:41 +0900

    tokenize_process_4th.c | here_doc이 여러 번 중첩되었을 때, 같은 파일에서 작성하는 것을 수정

--- srcs/parser/tokenize_process_4th.c
+++ srcs/parser/tokenize_process_4th.c
@@ -1,7 +1,30 @@
 #include "minishell.h"
 
+static int	open_heredoc_fd(char *tmp, t_redir *new)
+{
+	int 	i;
+	int		fd;
+	char	*tmp2;
+
+	i = -1;
+	while (access(tmp, F_OK) == 0)
+	{
+		tmp2 = ft_strdup_s(tmp);
+		free(tmp);
+		tmp = ft_strjoin(tmp2, "1");
+		free(tmp2);
+	}
+	fd = open(tmp, O_RDWR | O_CREAT, 0644);
+	if (fd == -1)
+		ft_open_error();
+	while (tmp[++i])
+		new->file_name[i] = tmp[i];
+	free(tmp);
+	return (fd);
+}
+
 static int	ft_here_doc_pretreatment(t_p_data *pdata, \
-t_redir *new, t_word *word, int i)
+t_redir *new, t_word *word)
 {
 	char	*str;
 	char	*tmp;
@@ -18,14 +41,9 @@ t_redir *new, t_word *word, int i)
 	str = ft_itoa(++(word->tmp_num));
 	if (!str)
 		ft_allocation_error();
-	tmp = ft_strjoin_s(".tmp", str);
+	tmp = ft_strjoin_s("/tmp/.tmp", str);
 	free(str);
-	new->heredoc_fd = open(tmp, O_RDWR | O_CREAT, 0644);
-	if (!new->heredoc_fd)
-		ft_open_error();
-	while (tmp[++i])
-		new->file_name[i] = tmp[i];
-	free(tmp);
+	new->heredoc_fd = open_heredoc_fd(tmp, new);
 	return (0);
 }
 
@@ -51,7 +69,7 @@ int	ft_redirect_here_doc(t_p_data *pdata, t_redir *new, t_word *word)
 {
 	char	*tmp;
 
-	if (ft_here_doc_pretreatment(pdata, new, word, -1) == 1)
+	if (ft_here_doc_pretreatment(pdata, new, word) == 1)
 		return (1);
 	tmp = ft_strdup_s(word->word + word->re_idx);
 	word->re_idx -= 2;

c3bf3b638717f0b8ee3ccc8956ba90db7990e5ac
Kdelphinus <kmj951015@gmail.com>
2023-04-14 22:39:14 +0900

    signal_func.c | signal_handler_for_parrent 함수 추가

--- srcs/parser/signal_func.c
+++ srcs/parser/signal_func.c
@@ -1,5 +1,16 @@
 #include "minishell.h"
 
+void	signal_handler_for_parrent(int sig)
+{
+	if (sig == SIGINT)
+	{
+		g_exit_status = 1;
+		rl_replace_line("", 0);
+		printf("\n");
+		rl_redisplay();
+	}
+}
+
 void	signal_handler(int sig)
 {
 	char	*tmp;
@@ -9,7 +20,7 @@ void	signal_handler(int sig)
 		g_exit_status = 1; // TODO 빈 프롬프트에 SIGINT를 넣으면 종료상태가 1로 변함, bash가
 		rl_replace_line("", 0);
 		printf("\n");
-		rl_on_new_line();
+		rl_on_new_line(); // TODO 자식 기다릴 떄, 이 동작 있으면 안 됨
 		rl_redisplay();
 	}
 	if (sig == SIGQUIT)
@@ -17,9 +28,9 @@ void	signal_handler(int sig)
 		tmp = ft_itoa(g_exit_status);
 		if (!tmp)
 			ft_allocation_error();
-		write(STDERR_FILENO, "Quit: ", 6);
-		write(STDERR_FILENO, tmp, ft_strlen(tmp));
-		write(STDERR_FILENO, "\n", 1);
+		ft_putstr_fd("Quit: ", STDERR_FILENO);
+		ft_putstr_fd(tmp, STDERR_FILENO);
+		ft_putstr_fd("\n", STDERR_FILENO);
 		free_s(tmp);
 		exit(1);
 	}

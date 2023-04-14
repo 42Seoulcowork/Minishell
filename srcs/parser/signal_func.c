90c26b4e6ad4629c2c60471fb6922626112cd122
gyuhong <qjwb2@naver.com>
2023-04-14 14:30:48 +0900

    srcs/parser/signal_func.c | 주석 제거, g_exit_status 값을 130에서 1로 수정

--- srcs/parser/signal_func.c
+++ srcs/parser/signal_func.c
@@ -5,10 +5,10 @@ void	signal_handler(int sig)
 	g_exit_status += sig;
 	if (sig == SIGINT)
 	{
-		g_exit_status = 130;
+		g_exit_status = 1;
 		rl_replace_line("", 0);
-//		printf("\n");
-//		rl_on_new_line();
+		printf("\n");
+		rl_on_new_line();
 		rl_redisplay();
 	}
 	if (sig == SIGQUIT)

717f46c81993919cccb24be8da6636fa1a203fab
subillie <subinlee@c1r6s2.42seoul.kr>
2023-04-29 16:11:42 +0900

    ft_echo.c | 에러메시지 출력 문제 해결

--- srcs/built_in/ft_echo.c
+++ srcs/built_in/ft_echo.c
@@ -16,15 +16,15 @@ static int	check_echo_option(char *arg)
 	return (i);
 }
 
-static void	loop_echo(char **args, int *newline_flag)
+static int	loop_echo(char **args, char *str, int *newline_flag, int no_option)
 {
 	int	i;
 	int	j;
-	int	no_option;
+	int	str_index;
 	int	option_flag;
 
 	i = 0;
-	no_option = 0;
+	str_index = -1;
 	while (args[++i])
 	{
 		if (no_option == 0)
@@ -33,14 +33,40 @@ static void	loop_echo(char **args, int *newline_flag)
 		{
 			j = -1;
 			while (args[i][++j])
-				write(STDOUT_FILENO, &args[i][j], 1);
+				str[++str_index] = args[i][j];
 			if (args[i + 1])
-				write(STDOUT_FILENO, " ", 1);
+				str[++str_index] = ' ';
 			no_option = 1;
 		}
 		else
 			*newline_flag = 0;
 	}
+	return (str_index);
+}
+
+static void	print_args(char **args, int *newline_flag)
+{
+	int		i;
+	int		j;
+	int		str_index;
+	size_t	count;
+	char	*str;
+
+	i = 0;
+	j = -1;
+	count = 1;
+	while (args[++i])
+		count += ft_strlen(args[i]) + 1;
+	str = malloc_s(count);
+	i = -1;
+	while (str[++i])
+		str[i] = '\0';
+	*newline_flag = 1;
+	str_index = loop_echo(args, str, newline_flag, 0);
+	if (*newline_flag == 1)
+		str[++str_index] = '\n';
+	write(STDOUT_FILENO, str, ft_strlen(str));
+	free_s(str);
 }
 
 void	ft_echo(char **args)
@@ -53,8 +79,5 @@ void	ft_echo(char **args)
 		ft_putchar_fd('\n', STDOUT_FILENO);
 		return ;
 	}
-	newline_flag = 1;
-	loop_echo(args, &newline_flag);
-	if (newline_flag == 1)
-		write(STDOUT_FILENO, "\n", 1);
+	print_args(args, &newline_flag);
 }
